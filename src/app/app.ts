86aea9b1e612c1f409a2750712851bee7db3a0c1
Didier <matheus.didier@gmail.com>
2019-10-26 20:52:23 -0300

    Back resolvendo cross

--- src/app/app.ts
+++ src/app/app.ts
@@ -21,6 +21,12 @@ import {Main} from './main'
 
 const app: express.Application = express();
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.get('/pdf', function (req, res) {
     var json = {
         "censec": {
